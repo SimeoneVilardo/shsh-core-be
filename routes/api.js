1a81a27dfb75d61f24eabcefad94ed9f0a677e10
SimeoneVilardo <simeone.vilardo@gmail.com>
2022-02-03 01:06:10 +0100

    test commit

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ var secretHelper = require('../helpers/secret-helper');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.json({ message: 'Hello APIs' });
+  res.json({ message: 'Hello (new) APIs' });
 });
 
 router.post('/eren', async function (req, res, next) {
